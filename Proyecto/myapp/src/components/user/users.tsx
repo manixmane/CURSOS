6911995c2c25100aba4c47a89899a863713551ad
manixmane <german_ah_10@hotmail.com>
2020-01-24 17:36:14 -0600

    Update users.tsx

--- Proyecto/myapp/src/components/user/users.tsx
+++ Proyecto/myapp/src/components/user/users.tsx
@@ -15,12 +15,20 @@ import {
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ListAltIcon from '@material-ui/icons/ListAlt';
+import UserView from './user-view';
 
 type Props={}
 
 type State ={
     users:User[];
-    userIdSeleccionado:number;
+}
+
+const userDefault:User={
+  id:0,
+  first_name:"",
+  last_name:"",
+  email:"",
+  avatar:""
 }
 
 let theme = createMuiTheme();
@@ -31,8 +39,7 @@ class Users extends React.Component<Props, State>{
     constructor(props:Props){
         super(props);
         this.state={
-            users:[],
-            userIdSeleccionado:0
+            users:[]
         };
     }
 
@@ -43,84 +50,70 @@ class Users extends React.Component<Props, State>{
     render(){
     
         const {users} = this.state;
-
-        console.log(users);
         return (
-          users.length> 0 &&(
-            <TableContainer component={Paper}>
-              <Table style={{minWidth:"650"}} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Id</TableCell>
-                    <TableCell align="right">Nombre</TableCell>
-                    <TableCell align="right">Apellido</TableCell>
-                    <TableCell align="right">Correo</TableCell>
-                    <TableCell align="right">Avatar</TableCell>
-                    <TableCell align="right">Opciones</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {users.map(row => (
-                    <TableRow key={row.id}>
-                      <TableCell component="th" scope="row">{row.id}</TableCell>
-                      <TableCell align="right">{row.first_name}</TableCell>
-                      <TableCell align="right">{row.last_name}</TableCell>
-                      <TableCell align="right">{row.email}</TableCell>
-                      <TableCell align="right"><img src={row.avatar}></img></TableCell>
-                      <TableCell align="right">
-                        <IconButton aria-label="Editar" style={{margin:theme.spacing(1)}} onClick={()=>{
-                          if(row!==null){
-                            this.onClickEditar(row);
-                          }
-                        }}>
-                          <EditIcon fontSize="large"/>
-                        </IconButton>
-                        <IconButton aria-label="Eliminar" style={{margin:theme.spacing(1)}} onClick={()=>{
-                          if(row!==null){
-                            this.onClickEditar(row);
-                          }
-                        }}>
-                          <DeleteIcon fontSize="large"/>
-                        </IconButton>
-                        <IconButton aria-label="Consultar" style={{margin:theme.spacing(1)}} onClick={()=>{
-                          if(row!==null){
-                            this.onClickEditar(row);
-                          }
-                        }}>
-                          <ListAltIcon fontSize="large"/>
-                        </IconButton>
-                        </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          )
-          );
-        // return(
-        //      <div>
-        //      </div>
-        // );
+          users.length > 0 && (
+              <TableContainer component={Paper}>
+                    <Table style={{minWidth:"650"}} aria-label="simple table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Id</TableCell>
+                          <TableCell align="right">Nombre</TableCell>
+                          <TableCell align="right">Apellido</TableCell>
+                          <TableCell align="right">Correo</TableCell>
+                          <TableCell align="right">Avatar</TableCell>
+                          <TableCell align="right">Opciones</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {users.map(row => (
+                          <TableRow key={row.id}>
+                            <TableCell component="th" scope="row">{row.id}</TableCell>
+                            <TableCell align="right">{row.first_name}</TableCell>
+                            <TableCell align="right">{row.last_name}</TableCell>
+                            <TableCell align="right">{row.email}</TableCell>
+                            <TableCell align="right"><img src={row.avatar}></img></TableCell>
+                            <TableCell align="right">
+                              <IconButton aria-label="Editar" style={{margin:theme.spacing(1)}} onClick={()=>{
+                                if(row!==null){
+                                  this.onClickEditar(row);
+                                }
+                              }}>
+                                <EditIcon fontSize="large"/>
+                              </IconButton>
+                              <IconButton aria-label="Eliminar" style={{margin:theme.spacing(1)}} onClick={()=>{
+                                if(row!==null){
+                                  this.onClickEditar(row);
+                                }
+                              }}>
+                                <DeleteIcon fontSize="large"/>
+                              </IconButton>
+                              <UserView user={row}/>
+                              </TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+            )
+        );
     }
 
     obtenerUsuarios =async ()=>{
-        try{
-          console.log('Intancia servicio usuarios');
-          const usersServices = UsersServices.obtenerinstancia();
+      try{
+        const usersServices = UsersServices.obtenerinstancia();
         const response = await usersServices.obtenerUsuarios();
-        console.log('resonse',response.data);
         if(response.data.length>0){
             this.setState({
-                users:response.data
+              users:response.data
             });
         }
-        }catch(error){
-          console.log('error:',error);
-        }
+      }catch(error){
+        console.log(error);
+      }
     }
 
     onClickEditar = (user:User)=>{
-      console.log("Usuario seleccionado", user);
+      //console.log("Usuario seleccionado", user);
     }
 }
 
